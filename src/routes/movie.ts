654fd9a37d318c9fd6aed4b19907caf14bd2a826
Grzegorz Celek <g.celek@gmail.com>
2024-06-05 15:04:02 +0200

    remove get method (for testing)

--- src/routes/movie.ts
+++ src/routes/movie.ts
@@ -3,9 +3,6 @@ import { movieController } from "../modules/movies";
 
 const router: Router = express.Router();
 
-router
-  .route("/")
-  .post(movieController.get10SimilarMovies)
-  .get(movieController.get10SimilarMovies);
+router.route("/").post(movieController.get10SimilarMovies);
 
 export default router;
